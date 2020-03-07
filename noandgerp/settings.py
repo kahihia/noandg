297e5d33ca524a7bd36ffc6cc3df0957f00d8d3b
Joseph Nganga <kajoengasa@gmail.com>
2020-03-08 01:19:58 +0300

    Update settings.py

--- noandgerp/settings.py
+++ noandgerp/settings.py
@@ -91,13 +91,25 @@ WSGI_APPLICATION = 'noandgerp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+ "default": 
+ {
+  "ENGINE": "django.db.backends.postgresql_psycopg2", #one of those should work
+  'ENGINE': 'django.db.backends.postgresql',   #one of those should work
+  "NAME": 'myProject',
+  "HOST": "localhost", 
+  "PORT": "5432",
+ }
+ }
 
+ import dj_database_url 
+ DATABASES['default'] =  dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
